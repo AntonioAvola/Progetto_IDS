13e783d3ae8668a1b757dff70cdb945b67067f70
Antonio Avola <antonio.avola@studenti.unicam.it>
2024-08-23 12:33:28 +0200

    [ADD] Aggiunte repositories

--- Progetto_JAVA/src/main/java/com/unicam/Main.java
+++ Progetto_JAVA/src/main/java/com/unicam/Main.java
@@ -2,6 +2,8 @@ package com.unicam;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao mondo!");
+        System.out.println("Hello World!");
+
+
     }
 }
