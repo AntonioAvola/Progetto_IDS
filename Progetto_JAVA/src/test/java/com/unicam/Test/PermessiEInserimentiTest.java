53acca2c7c7ad4b9aa2820c7e429d85dae09be6e
ele-3-car <eleonora.carbini2003@gmail.com>
2024-08-28 16:10:57 +0200

    [ADD] Aggiunta Test PermessiEInserimentiTest

---
+++ Progetto_JAVA/src/test/java/com/unicam/Test/PermessiEInserimentiTest.java
@@ -0,0 +1,67 @@
+package com.unicam.Test;
+
+import com.unicam.Model.BuilderContenuto.ItinerarioBuilder;
+import com.unicam.Model.Itinerario;
+import com.unicam.Model.PuntoGeolocalizzato;
+import com.unicam.Model.Ruolo;
+import com.unicam.Model.User;
+import com.unicam.Repository.ContenutoRepository;
+import com.unicam.Service.ContenutoService;
+import com.unicam.dto.UtenteDTO;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermessiEInserimentiTest {
+
+    @Test
+    public void testAggiuntaContenutoFallita(){
+        User utente = new User("Eleonora", "ele.car@gmail.com", "EleCar3",
+                "Santa Vittoria", "ele-3-car");
+        utente.setRuolo(Ruolo.CONTRIBUTOR);
+        UtenteDTO autore = new UtenteDTO(utente.getName(), utente.getComune(), utente.getRuolo());
+
+        List<PuntoGeolocalizzato> punti = new ArrayList<PuntoGeolocalizzato>();
+        punti.add(new PuntoGeolocalizzato());
+
+        ItinerarioBuilder builder = new ItinerarioBuilder();
+        builder.BuildAutore(autore);
+        builder.BuildTitolo("Corsa");
+        builder.BuildDescrizione("Corsetta semplice in mezzo alla natura");
+        builder.BuildSpecifica(punti);
+        Itinerario itinerio = builder.Result();
+
+        ContenutoRepository<Itinerario> itinerarioRepo = new ContenutoRepository<>();
+        ContenutoService<Itinerario> service = new ContenutoService<>(itinerarioRepo);
+
+        assertThrows(UnsupportedOperationException.class, () -> {
+            service.AggiungiContenuto(utente, itinerio);
+        });
+    }
+
+    @Test
+    public void testAggiuntaContenuto(){
+        User utente = new User("Antonio", "anto.avo@gmail.com", "AntoAvo2",
+                "Castelfidardo", "anto-2-avo");
+        utente.setRuolo(Ruolo.CONTRIBUTOR_AUTORIZZATO);
+        UtenteDTO autore = new UtenteDTO(utente.getName(), utente.getComune(), utente.getRuolo());
+
+        List<PuntoGeolocalizzato> punti = new ArrayList<PuntoGeolocalizzato>();
+        punti.add(new PuntoGeolocalizzato());
+
+        ItinerarioBuilder builder = new ItinerarioBuilder();
+        builder.BuildAutore(autore);
+        builder.BuildTitolo("Corsa");
+        builder.BuildDescrizione("Corsetta semplice in mezzo alla natura");
+        builder.BuildSpecifica(punti);
+        Itinerario itinerio = builder.Result();
+
+        ContenutoRepository<Itinerario> itinerarioRepo = new ContenutoRepository<>();
+        ContenutoService<Itinerario> service = new ContenutoService<>(itinerarioRepo);
+
+        service.AggiungiContenuto(utente, itinerio);
+    }
+
+}
--- Progetto_JAVA/src/test/java/com/unicam/Test/primoTest.java
+++
@@ -1,4 +0,0 @@
-package com.unicam.Test;
-
-public class primoTest {
-}
