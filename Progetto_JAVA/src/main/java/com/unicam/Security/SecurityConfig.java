833933ff79a53566abdad00c8621c164ceeed5f4
Antonio Avola <antonio.avola@studenti.unicam.it>
2024-09-13 12:23:26 +0200

    [FIXED] SecurityConfig.java

--- Progetto_JAVA/src/main/java/com/unicam/Security/SecurityConfig.java
+++ Progetto_JAVA/src/main/java/com/unicam/Security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
             "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
             "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
-            "/api/test/**", "/authenticate", "/api/utente/RegistrazioneUtente", "/Api/GestorePiattaforma/Registrazione" };
+            "/api/test/**", "/authenticate", "/api/utente/Api/RegistrazioneUtente", "/Api/GestorePiattaforma/Registrazione" };
 
     @Autowired
     private final JwtRequestFilter jwtRequestFilter;
